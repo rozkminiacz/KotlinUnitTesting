350065a60a204c3e36c15b74b4c11bd91c7071bc
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-11 12:33:32 +0200

    Feedback: use Input class

--- kotlintest/src/test/kotlin/me/rozkmin/testing/kotlintest/DistanceConverterParameterizedTest.kt
+++ kotlintest/src/test/kotlin/me/rozkmin/testing/kotlintest/DistanceConverterParameterizedTest.kt
@@ -1,6 +1,6 @@
 package me.rozkmin.testing.kotlintest
 
-import io.kotlintest.forAll
+import io.kotlintest.inspectors.forAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import me.rozkmin.testing.application.DistanceConverter
@@ -9,21 +9,27 @@ import me.rozkmin.testing.application.Meter
 
 
 class DistanceConverterParameterizedTest : StringSpec({
+
   val converter = DistanceConverter()
 
   fun Meter.toKilometer(): Kilometer =
     converter.parse(this)
 
   "Convert meter to kilometer" {
-    val testCases = listOf(
-      500L to 0.5,
-      750L to 0.8
+    val inputs = listOf(
+      Input(500L, 0.5),
+      Input(750L, 0.8)
     )
 
-    forAll(testCases) { (meter, kilometer) ->
+    inputs.forAll { (meter, kilometer) ->
       meter.toKilometer() shouldBe kilometer
     }
   }
 
 
 })
+
+
+data class Input(val parameter: Meter, val expected: Kilometer)
+
+
