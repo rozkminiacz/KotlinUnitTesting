70d93b9dc032acbdcdbe1d8dfcf3cb6fd41d4f02
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-11 12:09:17 +0200

    More of the same while converting :spek

---
+++ spek/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    kotlin("jvm")
+}
+group = "me.rozkmin.testing"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(project(":application"))
+    testImplementation (Libs.spek_dsl_jvm)  {
+        exclude("org.jetbrains.kotlin")
+    }
+    testRuntimeOnly (Libs.spek_runner_junit5) {
+        exclude("org.junit.platform")
+        exclude("org.jetbrains.kotlin")
+    }
+
+    testCompile(Libs.junit_jupiter_api)
+
+
+    // spek requires kotlin-reflect, can be omitted if already in the classpath
+    testRuntimeOnly(Libs.kotlin_reflect)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    maven {
+        setUrl("https://dl.bintray.com/spekframework/spek-dev")
+    }
+}
+
+
+tasks.withType<Test>().configureEach {
+    this.useJUnitPlatform {
+        includeEngines("spek2")
+    }
+}
