6f1573dfa91f2da01df1d5b604875f9ec0da9201
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-22 14:20:36 +0200

    Convert new projects to Gradle Kotlin/DSL

---
+++ mockito-kotlin/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  application
+  kotlin("jvm")
+}
+
+dependencies {
+  compile(Libs.kotlin_stdlib_jdk8)
+  implementation(project(":application"))
+  testCompile(Libs.mockito_core)
+  testCompile(Libs.mockito_kotlin)
+
+}
